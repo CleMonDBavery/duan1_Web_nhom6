8397ec1d379d5aca2ec78e03e23f72c13bb64c30
KIEN <kienap12f@gmail.com>
2023-11-27 14:08:20 +0700

    NO9 - chức năng order và orderdetail

--- admin/page/tableDetailOrder.php
+++ admin/page/tableDetailOrder.php
@@ -9,46 +9,49 @@
                 <div class="card">
                     <div class="card-header">
                         <h2 class="card-title">
-                            Quản lí hóa đơn
+                            Quản lí chi tiết hóa đơn
                         </h2>
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table" id="table1">
+                            <table class="table">
                                 <thead>
                                 <tr>
                                     <th>Tên sản phẩm</th>
                                     <th>Giá</th>
-                                    <th>Xóa</th>
+                                    <th>Số lượng</th>
+                                    <th style="text-align: right; padding: 0;">Tổng giá</th>
                                 </tr>
                                 </thead>
-                                <?
+                                <tbody>
+                                <?php
                                 $orderId = $_GET['idOrder'];
                                 $order = new orders();
                                 $product = new products();
                                 $list = $order->getOrderDetail($orderId);
-                                foreach ($list as $item) { ?>
-                                    <tbody>
+                                $grandTotal = 0; // Initialize grand total
+
+                                foreach ($list as $item) {
+                                    $subtotal = $item['amount'] * $item['price'];
+                                    $grandTotal += $subtotal;
+                                    ?>
                                     <tr>
                                         <td><?= $item['name'] ?></td>
-
-                                        <td>
-                                            <a class="btn btn-warning"
-                                               href="?page=hiddenOrderDetail&idHidden=<?= $item['orderDetailId'] ?>"
-                                               class="btn btn-info">Ẩn</a>
+                                        <td><?= $item['price'] ?></td>
+                                        <td><?= $item['amount'] ?></td>
+                                        <td style="text-align: right; padding: 0;"><?= number_format($subtotal) ?>VND
                                         </td>
                                     </tr>
-                                    </tbody>
-                                <? }
-                                ?>
+                                <?php } ?>
+                                </tbody>
                             </table>
+                            <div>
+                                <strong>Tổng cộng:</strong> <?= number_format($grandTotal) ?> VND
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </section>
-            <!-- Basic Tables end -->
-
         </div>
 
         <?php
